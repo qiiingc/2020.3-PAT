b37f19ccf1e865a95a3223bcc3ec1e06def97ca3
qiiingc <ching_shing@sjtu.edu.cn>
2019-12-25 00:50:57 +0800

    ini

---
+++ TestPAT/TestPAT/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TestPAT
+//
+//  Created by chingshing on 2019/12/25.
+//  Copyright © 2019 com.sjtu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

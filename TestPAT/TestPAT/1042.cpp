d6f1bc039370df1bf4c7fa561c0f8f8765629b8a
qiiingc <ching_shing@sjtu.edu.cn>
2019-12-25 09:05:59 +0800

    第三版，满分答案

--- TestPAT/TestPAT/1042.cpp
+++ TestPAT/TestPAT/1042.cpp
@@ -3,17 +3,18 @@ int main(){
     freopen("/Users/ching_shing/Documents/SJTU/ky/PAT顶级/PAT-GitHub/2020.3-PAT/TestPAT/TestPAT/1042.in","r", stdin);
     int n,x;
     cin>>n;
-    int a[55],b[55];
+    int a[55],b[55],c[55];
     for(int i=1;i<=54;i++){
         cin>>x;
         a[x]=i;
+        c[i]=x;
     }
     for(;n>1;n--){
         for(int i=1;i<=54;i++){
             b[i]=a[i];
         }
         for(int i=1;i<=54;i++){
-            a[b[i]]=i;
+            a[c[i]]=b[i];
         }
     }
     for(int i=1;i<=54;i++){
@@ -26,8 +27,8 @@ int main(){
         else if(y==4)cout<<"J";
         cout<<(a[i]-1)%13+1;
     }
-    cout<<endl;
-    cout<<"S7 C11 C10 C12 S1 H7 H8 H9 D8 D9 S11 S12 S13 D10 D11 D12 S3 S4 S6 S10 H1 H2 C13 D2 D3 D4 H6 H3 D13 J1 J2 C1 C2 C3 C4 D1 S5 H5 H11 H12 C6 C7 C8 C9 S2 S8 S9 H10 D5 D6 D7 H4 H13 C5"<<endl;
+//    cout<<endl;
+//    cout<<"S7 C11 C10 C12 S1 H7 H8 H9 D8 D9 S11 S12 S13 D10 D11 D12 S3 S4 S6 S10 H1 H2 C13 D2 D3 D4 H6 H3 D13 J1 J2 C1 C2 C3 C4 D1 S5 H5 H11 H12 C6 C7 C8 C9 S2 S8 S9 H10 D5 D6 D7 H4 H13 C5"<<endl;
     return 0;
 }
 

fbe23bc82c11f16bea83579f603710d192866069
qiiingc <ching_shing@sjtu.edu.cn>
2019-12-29 20:56:08 +0800

    第二版

--- TestPAT/TestPAT/1065.cpp
+++ TestPAT/TestPAT/1065.cpp
@@ -6,7 +6,9 @@ int main(){
     cin>>n;
     for(int i=0;i<n;i++){
         cin>>a>>b>>c;
-        printf("Case #%d: %s\n",i+1,a+b>c?"true":"false");
+        if(a>0&&b>0&&c<=0)printf("Case #%d: %s\n",i+1,"false");
+        else if(a<0&&b<0&&c>=0)printf("Case #%d: %s\n",i+1,"false");
+        else printf("Case #%d: %s\n",i+1,a+b>c?"true":"false");
     }
     
     return 0;

ef0f29f81902610cf72be0975dbd959b110f8d16
qiiingc <ching_shing@sjtu.edu.cn>
2020-01-07 20:10:45 +0800

    第四版，20分满分

--- TestPAT/TestPAT/1136.cpp
+++ TestPAT/TestPAT/1136.cpp
@@ -2,7 +2,6 @@
 int main(){
     freopen("/Users/ching_shing/Documents/SJTU/ky/PAT顶级/PAT-GitHub/2020.3-PAT/TestPAT/TestPAT/1136.in","r", stdin);
     string s,r,k,t;
-    long long p;
     cin>>s;
     r=s;
     reverse(r.begin(), r.end());
@@ -12,30 +11,23 @@ int main(){
     }
     bool f=false;
     for(int i=0;i<10;i++){
-        if(s.size()>18){
-            k="";
-            for(int i=0;i<s.size();i++){
-                k+=to_string((s[i]-'0')+(r[i]-'0'));
-            }
-            t=k;
-            reverse(t.begin(), t.end());
-            if(k==t){
-                cout<<s<<" + "<<r<<" = "<<k<<endl;
-                cout<<k<<" is a palindromic number.";
-                f=true;
-                break;
-            }
-        }else{
-            p=stoll(s)+stoll(r);
-            cout<<s<<" + "<<r<<" = "<<p<<endl;
-            s=to_string(p);
-            r=s;
-            reverse(r.begin(), r.end());
-            if(r==s){
-                cout<<p<<" is a palindromic number.";
-                f=true;
-                break;
-            }
+        k="";
+        int carry=0,x;
+        for(int i=0;i<s.size();i++){
+            x=(s[i]-'0')+(r[i]-'0');
+            k+=to_string((x%10+carry)%10);
+            carry=x/10+(x%10+carry)/10;
+        }
+        if(carry!=0)k+=to_string(carry);
+        cout<<s<<" + "<<r<<" = ";
+        r=k;
+        s=r;
+        reverse(s.begin(), s.end());
+        cout<<s<<endl;
+        if(s==r){
+            cout<<k<<" is a palindromic number.";
+            f=true;
+            break;
         }
     }
     if(!f){

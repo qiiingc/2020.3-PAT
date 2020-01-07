d22a36a01a7d3d5c7db1f1d8effaf0769fe3f507
qiiingc <ching_shing@sjtu.edu.cn>
2020-01-07 19:19:25 +0800

    第一版，14分，过了3/7个测试点

---
+++ TestPAT/TestPAT/1136.cpp
@@ -0,0 +1,26 @@
+#include "0.h"
+int main(){
+    freopen("/Users/ching_shing/Documents/SJTU/ky/PAT顶级/PAT-GitHub/2020.3-PAT/TestPAT/TestPAT/1136.in","r", stdin);
+    string s,r;
+    long long p;
+    cin>>s;
+    r=s;
+    reverse(r.begin(), r.end());
+    bool f=false;
+    for(int i=0;i<10;i++){
+        p=stoll(s)+stoll(r);
+        cout<<s<<" + "<<r<<" = "<<p<<endl;
+        s=to_string(p);
+        r=s;
+        reverse(r.begin(), r.end());
+        if(r==s){
+            cout<<p<<" is a palindromic number."<<endl;
+            f=true;
+            break;
+        }
+    }
+    if(!f){
+        cout<<"Not found in 10 iterations."<<endl;
+    }
+    return 0;
+}

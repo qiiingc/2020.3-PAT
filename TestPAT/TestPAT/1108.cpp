f47707ecb5a319734b6fdbbf6b522bf913fb1e2f
qiiingc <ching_shing@sjtu.edu.cn>
2020-01-10 11:11:33 +0800

    第一版，0分

---
+++ TestPAT/TestPAT/1108.cpp
@@ -0,0 +1,50 @@
+#include "0.h"
+int main(){
+    freopen("/Users/ching_shing/Documents/SJTU/ky/PAT顶级/PAT-GitHub/2020.3-PAT/TestPAT/TestPAT/1108.in","r", stdin);
+    int n,num=0;
+    double total=0.0;
+    cin>>n;
+    string s;
+    for(int i=0;i<n;i++){
+        cin>>s;
+        bool f=true;
+        int dot=0;
+        int len=s.size();
+        for(int j=0;j<len;j++){
+            if(s[j]!='-' && s[j]!='.' && !isdigit(s[j])){
+                f=false;
+            }
+            if(s[j]=='.'){
+                dot++;
+            }
+        }
+        if(dot>1){
+            f=false;
+        }
+        if(f && (stof(s)>1000.0 || stof(s)<-1000.0)){
+            f=false;
+        }
+        if(f){
+            int pos=s.find('.');
+            if(pos!=-1){
+                if(len-pos>3){
+                    f=false;
+                }
+            }
+        }
+        if(!f){
+            cout<<"ERROR: "<<s<<" is not a legal number"<<endl;
+        }else{
+            num++;
+            total+=stod(s);
+        }
+    }
+    if(num==0){
+        cout<<"The average of 0 number is Undefined";
+    }else if(num==1){
+        cout<<"The average of 1 number is "<<total;
+    }else{
+        printf("The average of %d number is %.2f",num,(double)total/num);
+    }
+    return 0;
+}

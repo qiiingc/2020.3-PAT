333ac8ecdcc4e27a94a6ff303ee587787d250e66
qiiingc <ching_shing@sjtu.edu.cn>
2020-01-13 10:49:29 +0800

    第二版，待提交，已经修补了间隔符为空格或者为非回车的情况

--- TestPAT/TestPAT/1164.cpp
+++ TestPAT/TestPAT/1164.cpp
@@ -1,10 +1,5 @@
 #include "0.h"
 int n,m;
-//int a[maxn];
-//struct node{
-//    int a[7][7];
-//};
-//vector<node> v[26];
 char v[26][7][5];
 void prtarr(int w,int x,int s,int e){
     cout<<w<<" "<<x<<endl;
@@ -17,38 +12,28 @@ int main(){
     freopen("/Users/ching_shing/Documents/SJTU/ky/PAT顶级/PAT-GitHub/2020.3-PAT/TestPAT/TestPAT/1164.in","r", stdin);
     for(int i=0;i<26;i++){
        for(int j=0;j<7;j++){
-           for(int k=0;k<5;k++){
-                scanf("%c",&v[i][j][k]);
-                //printf("%c",v[i][j][k]);
-            }
-            getchar();
-            //cout<<endl;
+           scanf("%c%c%c%c%c\n",&v[i][j][0],&v[i][j][1],&v[i][j][2]
+               ,&v[i][j][3],&v[i][j][4]);
         }
     }
     char ch;
     bool f=false;
     vector<int> word;
-    while(cin>>ch){
+    while(~scanf("%c",&ch)){
         if(isupper(ch)){
             word.push_back(ch-'A');
-        }else {
-            //cout<<word[0]<<" "<<word[1]<<endl;
+        }else if(!word.empty()||ch!='\n'){
             if(f)printf("\n");
             int len=word.size()*6;
             char w[7][len];
             for(int i=0;i<7;i++){
-                //cout<<word[k]+'A';
-                //printf("%c",word[k]+'A');
                 int k=0;
                 for(int j=0;j<len;j++){
-
-                        if(j>0&& (j+1)%6==0){
-                            w[i][j]=' ';
-                            k++;
-                        }
-                        else w[i][j]=v[word[k]][i][j%6];
-
-
+                    if(j>0&& (j+1)%6==0){
+                        w[i][j]=' ';
+                        k++;
+                    }
+                    else w[i][j]=v[word[k]][i][j%6];
                 }
             }
             for(int i=0;i<7;i++){

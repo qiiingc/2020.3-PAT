ac08c668ec15e68d5ba05d3954bc222cd2fc338b
qiiingc <ching_shing@sjtu.edu.cn>
2020-01-27 18:49:19 +0800

    第三版，30分满分

--- TestPAT/TestPAT/1167.cpp
+++ TestPAT/TestPAT/1167.cpp
@@ -1,17 +1,21 @@
 #include "0.h"
-int n,m,k,maxx=0;
+int n,m,k;
 vector<int> v;
-int t[10000];
-void check(int i,int low,int high){
-    if(low<0||low>high)return;
+struct tree{
+    int data;
+    tree *l,*r;
+}*T;
+tree* check(int low,int high){
+    if(low<0||low>high)return NULL;
+    tree *t=new tree;
     int minn=low;
     for(int j=low;j<=high;j++){
         if(v[minn]>v[j])minn=j;
     }
-    if(i>maxx)maxx=i;
-    t[i]=v[minn];
-    check(2*i,low,minn-1);
-    check(2*i+1,minn+1,high);
+    t->data=v[minn];
+    t->l=check(low,minn-1);
+    t->r=check(minn+1,high);
+    return t;
 }
 int main(){
     freopen("/Users/ching_shing/Documents/SJTU/ky/PAT顶级/PAT-GitHub/2020.3-PAT/TestPAT/TestPAT/1167.in","r", stdin);
@@ -20,15 +24,18 @@ int main(){
         scanf("%d",&k);
         v.push_back(k);
     }
-    check(1,0,n-1);
-    
+    T=check(0,n-1);
     int flag=0;
-    for(int i=1;i<=maxx;i++){
-        if(t[i]>0){
-            if(flag==1)cout<<" ";
-            flag=1;
-            cout<<t[i];
-        }
+    queue<tree*> q;
+    q.push(T);
+    while(!q.empty()){
+        tree *t=q.front();
+        if(flag==1)cout<<" ";
+        else flag=1;
+        cout<<t->data;
+        if(t->l)q.push(t->l);
+        if(t->r)q.push(t->r);
+        q.pop();
     }
     return 0;
 }

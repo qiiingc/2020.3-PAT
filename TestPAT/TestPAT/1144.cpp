b67ab2f02e968426b545a288d08afbd84c661d8f
qiiingc <ching_shing@sjtu.edu.cn>
2020-02-11 19:41:50 +0800

    1144 满分

--- TestPAT/TestPAT/1144.cpp
+++ TestPAT/TestPAT/1144.cpp
@@ -5,9 +5,9 @@ freopen("/Users/ching_shing/Documents/SJTU/ky/PAT顶级/PAT-GitHub/2020.3-PAT/Te
     scanf("%d\n",&n);
     while(n--){
         scanf("%d",&i);
-        if(i>0)a[i]=1;
+        if(i>0 && i<100001)a[i]=1;
     }
-    for(i=1;i<=100000;i++){
+    for(i=1;i<=100001;i++){
         if(a[i]!=1){
             cout<<i;
             break;

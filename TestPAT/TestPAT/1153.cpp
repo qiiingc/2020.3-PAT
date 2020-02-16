cdfac82d5ac4b9be17035062796a7dee5f0de352
qiiingc <ching_shing@sjtu.edu.cn>
2020-02-16 15:54:17 +0800

    1153 25/25

--- TestPAT/TestPAT/1153.cpp
+++ TestPAT/TestPAT/1153.cpp
@@ -1,48 +1,45 @@
 #include "0.h"
-int n,m,k,u,w;
+int n,m,k,num=0,total=0;
 string s;
-map<string,int> mp;
 struct node{
     string id;
-    int score;
+    int value;
 };
 bool cmp(node &a,node &b){
-    return a.score==b.score?a.id<b.id:a.score>b.score;
+    return a.value==b.value?a.id<b.id:a.value>b.value;
 }
 int main(){
     freopen("/Users/ching_shing/Documents/SJTU/ky/PAT顶级/PAT-GitHub/2020.3-PAT/TestPAT/TestPAT/1153.in","r", stdin);
     scanf("%d %d",&n,&m);
-    for(int i=1;i<=n;i++){
-        cin>>s>>k;
-        mp[s]=k;
+    vector<node> v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i].id>>v[i].value;
     }
     for(int i=1;i<=m;i++){
         cin>>k>>s;
         printf("Case %d: %d %s\n",i,k,s.c_str());
-        map<string,int>::iterator it;
+        vector<node> ans;
         if(k==1){
-            vector<node> v;
-            for(it=mp.begin();it!=mp.end();it++){
-                string x=it->first;
-                if(x.substr(0,1)==s){
-                    v.push_back({x,it->second});
+            for(auto it:v){
+                if(it.id.substr(0,1)==s){
+                    ans.push_back(it);
                 }
             }
-            if(v.size()==0){
+            if(ans.size()==0){
                 printf("NA\n");
                 continue;
             }
-            sort(v.begin(),v.end(),cmp);
-            for(auto j:v){
-                printf("%s %d\n",j.id.c_str(),j.score);
+            sort(ans.begin(),ans.end(),cmp);
+            for(auto j:ans){
+                printf("%s %d\n",j.id.c_str(),j.value);
             }
         }else if(k==2){
-            int num=0,total=0;
-            for(it=mp.begin();it!=mp.end();it++){
-                string x=it->first;
-                if(x.substr(1,3)==s){
+            num=0;
+            total=0;
+            for(auto it:v){
+                if(it.id.substr(1,3)==s){
                     num++;
-                    total+=it->second;
+                    total+=it.value;
                 }
             }
             if(num==0){
@@ -51,27 +48,22 @@ int main(){
             }
             printf("%d %d\n",num,total);
         }else if(k==3){
-            vector<node> v;
-            for(it=mp.begin();it!=mp.end();it++){
-                string x=it->first;
-                bool f=false;
-                if(x.substr(4,6)==s){
-                    for(auto j:v){
-                        if(j.id==x.substr(1,3)){
-                            it->second++;
-                            f=true;
-                        }
-                    }
-                    if(!f)v.push_back({x.substr(1,3),1});
+            unordered_map<string, int> ans3;
+            for(auto it:v){
+                if(it.id.substr(4,6)==s){
+                    ans3[it.id.substr(1,3)]++;
                 }
             }
-            if(v.size()==0){
+            if(ans3.size()==0){
                 printf("NA\n");
                 continue;
             }
-            sort(v.begin(),v.end(),cmp);
-            for(auto j:v){
-                printf("%s %d\n",j.id.c_str(),j.score);
+            for(auto it:ans3){
+                ans.push_back({it.first,it.second});
+            }
+            sort(ans.begin(),ans.end(),cmp);
+            for(auto j:ans){
+                printf("%s %d\n",j.id.c_str(),j.value);
             }
         }else {
             printf("NA\n");
